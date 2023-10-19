c44c93c919f41a85c38a72b3e0379e83d98829e6
Sweta Rajbhar <swetarajbhar3@gmail.com>
2023-10-19 20:25:26 +0530

    Added Java Program to print integer equivalent of a Roman Number

---
+++ JAVA/RomanToInteger.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+
+public class RomanToInteger {
+
+    static int convertRomanToInteger(String roman) {
+        int result = 0;
+        int prevValue = 0;
+
+        for (int i = roman.length() - 1; i >= 0; i--) {
+            char currentChar = roman.charAt(i);
+            int currValue = 0;
+
+            switch (currentChar) {
+                case 'I':
+                    currValue = 1;
+                    break;
+                case 'V':
+                    currValue = 5;
+                    break;
+                case 'X':
+                    currValue = 10;
+                    break;
+                case 'L':
+                    currValue = 50;
+                    break;
+                case 'C':
+                    currValue = 100;
+                    break;
+                case 'D':
+                    currValue = 500;
+                    break;
+                case 'M':
+                    currValue = 1000;
+                    break;
+            }
+
+            if (currValue < prevValue) {
+                result -= currValue;
+            } else {
+                result += currValue;
+            }
+
+            prevValue = currValue;
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Enter Roman Number :");
+        Scanner sc = new Scanner(System.in);
+        String roman = sc.nextLine();
+        sc.close();
+
+        int result = convertRomanToInteger(roman);
+        System.out.println("Result " + result);
+    }
+}
